bad73d8f64e7cc34a717384d3b52a044603288f1
Prolgu <mjolner_@hotmail.com>
2021-11-30 00:22:59 -0300

    Escrito el about me en la pagina

--- assets/js/master.js
+++ assets/js/master.js
@@ -3,14 +3,14 @@
 const cont = document.getElementById("cont")
 const welc = document.getElementById("welcome")
 const winSizeCss = () => {
-    let w = document.documentElement.clientWidth;
+    let w = document.documentElement.clientWidth
     if (w <= 500) {
-        cont.classList.add('fake-glass');
-        welc.classList.add('fake-glass');
-        // console.log(w);
+        cont.classList.add('fake-glass')
+        welc.classList.add('fake-glass')
+        // console.log(w)
     } else {
-        cont.classList.remove('fake-glass');
-        welc.classList.remove('fake-glass');
+        cont.classList.remove('fake-glass')
+        welc.classList.remove('fake-glass')
     }
 }
 
@@ -19,44 +19,49 @@ const arrw = document.getElementById("arrowUp")
 
 const arrowUp = (a = false) => {
     if (a === true) {
-        arrw.style.visibility = "visible";
-        arrw.classList.add('arrow-up');
+        arrw.style.visibility = "visible"
+        arrw.classList.add('arrow-up')
     } else {
-        arrw.style.visibility = "hidden";
-        arrw.classList.remove('arrow-up');
+        arrw.style.visibility = "hidden"
+        arrw.classList.remove('arrow-up')
     }
 }
 const scrollPosPerc = () => {
     //cursor actual
-    let scrollTop = window.scrollY;
+    let scrollTop = window.scrollY
     // cursor total
-    let docHeight = document.body.offsetHeight;
+    let docHeight = document.body.offsetHeight
     // let winHeight = window.innerHeight;// total de la ventana
     //redondeo del scroll actual
-    let scrollPercent = Math.round(scrollTop / docHeight * 100);
-    scrollPercent >= 60 ? arrowUp(true) : arrowUp();
+    let scrollPercent = Math.round(scrollTop / docHeight * 100)
+    scrollPercent >= 60 ? arrowUp(true) : arrowUp()
 
 }
 
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", mobileMenu)
+
+function mobileMenu() {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+}
+
+
 // consulto  alcarcar el script
-winSizeCss();
+winSizeCss()
 
 // para empezar sin ver la flecha
-arrowUp();
+arrowUp()
 
 
 // listener para saber consultar en resize window <= '500px'
-window.addEventListener("resize", winSizeCss);
+window.addEventListener("resize", winSizeCss)
 
 //listener para el scroll
 window.addEventListener("scroll", scrollPosPerc)
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
+console.groupCollapsed('No mirar')
+console.log('Dije que no miraras');
+console.groupEnd();
