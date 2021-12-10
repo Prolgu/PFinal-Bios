bf41baf2baed47942133708d6a624338ce40053c
Prolgu <mjolner_@hotmail.com>
2021-12-10 09:56:11 -0300

    Implementado el index para la seccion blog

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,66 +1,72 @@
 //=====================================================================
 // funcion para conseguir el ancho de pagina
 // al llegar a <= 500px agrega la clase fake-div al elemento cont
-const cont = document.getElementById("cont")
-const welc = document.getElementById("welcome")
+const cont = document.getElementById("cont");
+const welc = document.getElementById("welcome");
 
 const winSizeCss = () => {
-    let w = document.documentElement.clientWidth
-    if (w <= 500) {
-        cont.classList.add('fake-glass')
-        welc.classList.add('fake-glass')
+    let pageWidth = document.documentElement.clientWidth;
+    if (pageWidth <= 500) {
+        if (cont) cont.classList.add('fake-glass');
+        welc.classList.add('fake-glass');
         // console.log(w)
     } else {
-        cont.classList.remove('fake-glass')
-        welc.classList.remove('fake-glass')
+        if (cont) cont.classList.remove('fake-glass');
+        welc.classList.remove('fake-glass');
     }
 }
 //=====================================================================
-const arrw = document.getElementById("arrowUp")
+const arrw = document.getElementById("arrowUp");
 
 const arrowUp = (a = false) => {
     if (a === true) {
-        arrw.classList.add('arrow-up')
-        arrw.style.visibility = "visible"
+        arrw.classList.add('arrow-up');
+        arrw.style.visibility = "visible";
     } else {
-        arrw.style.visibility = "hidden"
-        arrw.classList.remove('arrow-up')
+        arrw.style.visibility = "hidden";
+        arrw.classList.remove('arrow-up');
     }
 }
 const scrollPosPerc = () => {
     //cursor actual
-    let scrollTop = window.scrollY
+    let scrollTop = window.scrollY;
     // cursor total
-    let docHeight = document.body.offsetHeight
+    let docHeight = document.body.offsetHeight;
     // let winHeight = window.innerHeight;// total de la ventana
     //redondeo del scroll actual
-    let scrollPercent = Math.round(scrollTop / docHeight * 100)
-    scrollPercent >= 60 ? arrowUp(true) : arrowUp()
-
+    let scrollPercent = Math.round(scrollTop / docHeight * 100);
+    scrollPercent >= 60 ? arrowUp(true) : arrowUp();
 }
 
 //=====================================================================
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 function mobileMenu() {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 //=====================================================================
 
 // consulto  alcarcar el script
-winSizeCss()
+winSizeCss();
 // para empezar sin ver la flecha
-arrowUp()
+arrowUp();
 // listener para saber consultar en resize window <= '500px'
-window.addEventListener("resize", winSizeCss)
+window.addEventListener("resize", winSizeCss);
 //listener para el scroll
-window.addEventListener("scroll", scrollPosPerc)
+window.addEventListener("scroll", scrollPosPerc);
 //evento para el menu hamburguesa
-hamburger.addEventListener("click", mobileMenu)
+if (hamburger) hamburger.addEventListener("click", mobileMenu);
 
 
+
+
+
+
+
+
+// Fold description {{{1 //
 console.groupCollapsed('No mirar')
 console.log(`
 ██████╗ ██████╗  ██████╗ ██╗      ██████╗ ██╗   ██╗
@@ -72,3 +78,4 @@ console.log(`
 Dije que no mirar, pero ya que estas aca: https://github.com/Prolgu/PFinal-Bios
     `);
 console.groupEnd()
+
